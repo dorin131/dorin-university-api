2bd781745a2ade7f0e778ae967cc4a23aceef978
dorin131 <dorin.fodor@gmail.com>
2018-12-26 21:03:30 +0000

    circleci config

--- scripts/test-ci.sh
+++ scripts/test-ci.sh
@@ -5,4 +5,4 @@ BRANCH="local"
 fi
 
 docker build -t dorin-university-backend:${BRANCH} .
-docker exec -it `docker container ps -q` bash -c "cd /srv && npm test"
+docker run -it --entrypoint "cd /srv && npm test" --env environment=prod dorin-university-backend
