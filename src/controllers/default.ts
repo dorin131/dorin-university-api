119d4b4acbd12571846c52590075aa1aaa456d60
dorin131 <dorin.fodor@gmail.com>
2018-12-26 17:48:54 +0000

    Added blank test. Added blank CircleCI config.

--- src/controllers/default.ts
+++ src/controllers/default.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
 
 export const healthCheck = (_: Request, res: Response) => {
-  res.json({ status: 'Under Construction.' });
+  res.json({ status: 'Under Construction' });
 };
